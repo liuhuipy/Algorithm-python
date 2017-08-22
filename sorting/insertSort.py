d38f127ec7429255edc018160afc914d32072aa2
liuhui <liuhui_py@163.com>
2017-08-22 19:49:45 +0800

    commit insertSort

---
+++ sorting/insertSort.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+__author__ = 'liuhui'
+
+
+'''
+插入排序
+'''
+
+class Solution:
+
+    def insertSort(self, listA):
+        for i in range(1, len(listA)):
+            midNum = listA[i]
+            j = i - 1
+            while j >= 0:
+                if listA[j] > midNum:
+                    listA[j+1], listA[j] = listA[j], midNum
+                    j -= 1
+                else:
+                    break
+        return listA
+
+
+if __name__ == '__main__':
+    listA = [3, 1, 5, 8, 6, 7, 4, 2, 10, 9]
+    solut = Solution()
+    newListA = solut.insertSort(listA)
+    print(newListA)
+
+
