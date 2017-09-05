29ee0b9c9033919582ebfd0c6011f1901077c1fb
liuhui <liuhui_py@163.com>
2017-09-05 23:41:11 +0800

    commit moreThanHalfNum

---
+++ 剑指offer/moreThanHalfNum.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+__author__ = 'liuhui'
+
+
+class Solution:
+
+    def MoreThanHalfNum_Solution(self, numbers):
+        # write code here
+        pass
+
+
+if __name__ == '__main__':
+    numbers = [1, 2, 3, 2, 2, 2, 5, 4, 2]
+    solut = Solution()
+    result = solut.MoreThanHalfNum_Solution(numbers)
+    print(result)
