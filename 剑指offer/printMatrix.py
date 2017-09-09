6e344e360fafe5340904212168c27dae32c633ff
liuhui <liuhui_py@163.com>
2017-09-09 12:19:57 +0800

    commit printMatrix

--- 剑指offer/printMatrix.py
+++ 剑指offer/printMatrix.py
@@ -4,19 +4,59 @@
 __author__ = 'liuhui'
 
 '''
-输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字，例如，如果输入如下矩阵： 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 
-则依次打印出数字1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10.
+输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字，
+例如，如果输入如下矩阵：
+[[ 1,  2,  3,  4],
+ [ 5,  6,  7,  8],
+ [ 9, 10, 11, 12],
+ [13, 14, 15, 16]]
+则依次打印出数字 1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10.
 '''
-
 
 class Solution:
     # matrix类型为二维列表，需要返回列表
     def printMatrix(self, matrix):
         # write code here
-        Y = len(array)
-        X = len(array[0])
-        choices = [(0,1),(1,0),(0,-1),(-1,0)]
-        
+        if len(matrix) <= 1:
+            return matrix[0]
+
+        resList = matrix[0]
+        matrix.remove(matrix[0])
+        choices = [(0,1),(-1,0),(0,-1),(1,0)]
+        i = 0
+        while matrix != []:
+
+            rotate = choices[i % 4]
+
+            if rotate == (0, 1):
+                Index = len(matrix[0]) - 1
+                for j in range(len(matrix)):
+                    resList.append(matrix[j][Index])
+                    del(matrix[j][Index])
+                if matrix[0] == []:
+                    for j in range(len(matrix)):
+                        matrix.remove(matrix[0])
+
+            elif rotate == (-1, 0):
+                Index = len(matrix) - 1
+                for k in range(len(matrix[0])-1,-1,-1):
+                    resList.append(matrix[Index][k])
+                matrix.remove(matrix[Index])
+
+            elif rotate == (0, -1):
+                Index = len(matrix) - 1
+                for m in range(Index,-1,-1):
+                    resList.append(matrix[m][0])
+                    del(matrix[m][0])
+                if matrix[0] == []:
+                    for j in range(len(matrix)):
+                        matrix.remove(matrix[0])
+            else:
+                for n in range(len(matrix[0])):
+                    resList.append(matrix[0][n])
+                matrix.remove(matrix[0])
+            i += 1
+        return resList
 
 
 
@@ -27,8 +67,11 @@ if __name__ == '__main__':
         [9, 10, 11, 12],
         [13, 14, 15, 16],
     ]
+    array1 = [[1]]
+    array2= [[1],[2],[3],[4],[5]]
+    array3 = [[1,2],[3,4],[5,6],[7,8],[9,10]]
     solut = Solution()
-    newList = solut.printMatrix(array)
+    newList = solut.printMatrix(array2)
     print(newList)
 
 
