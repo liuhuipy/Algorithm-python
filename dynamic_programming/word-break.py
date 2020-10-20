2d9b224e1ab3a61ed401ff5b8dc8ffffa6fc55e5
liuhuipy <liuhui_py@163.com>
2020-10-20 17:25:00 +0800

    Add word break

---
+++ dynamic_programming/word-break.py
@@ -0,0 +1,38 @@
+"""
+单词拆分：
+    给定一个非空字符串 s 和一个包含非空单词的列表 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+    说明：
+    拆分时可以重复使用字典中的单词。
+    你可以假设字典中没有重复的单词。
+示例 1：
+    输入: s = "leetcode", wordDict = ["leet", "code"]
+    输出: true
+    解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+示例 2：
+    输入: s = "applepenapple", wordDict = ["apple", "pen"]
+    输出: true
+    解释: 返回 true 因为 "applepenapple" 可以被拆分成 "apple pen apple"。
+         注意你可以重复使用字典中的单词。
+示例 3：
+    输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+    输出: false
+"""
+from typing import List
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        if not s:
+            return False
+        len_s = len(s)
+        dp = [False for _ in range(len_s + 1)]
+        dp[0] = True
+        for i in range(1, len_s + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+        return dp[-1]
+
+
+if __name__ == '__main__':
+    print(Solution().wordBreak("soseat", ["so", "eat", "so"]))
